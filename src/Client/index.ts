e248f6193cc559f07f11ba39b6bf1544797fab20
Birahe <ardaaozyigit@gmail.com>
2022-05-06 23:31:49 +0300

    Logger Without Colors

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,8 +1,8 @@
 import { Client, ClientOptions } from "discord.js";
-import Logger from "Logger";
+import Logger from "../Logger";
 
 export default class EarestClient extends Client {
-  public logger: Logger = new Logger();
+  public logger: Logger = new Logger(true, "logs");
   constructor(params: ClientOptions) {
     super(params);
   }
--- src/Logger/index.ts
+++ src/Logger/index.ts
@@ -1,10 +1,63 @@
 import { Logger as LoggerInterface } from "Interfaces/Logger";
+import { writeFileSync, appendFileSync, existsSync } from "fs";
 export default class Logger implements LoggerInterface {
   public format: string = "";
-  constructor() {}
-  debug(message: string, ...args: any[]): void {}
-  error(message: string, ...args: any[]): void {}
-  log(message: string, ...args: any[]): void {}
-  warn(message: string, ...args: any[]): void {}
-  setFormat(format: string): void {}
+  private logsDir: string = "";
+  private writeBool: boolean = false;
+  private fileName: string = "";
+  constructor(writeBool: boolean = false, logsDir: string = "logs") {
+    if (writeBool) {
+      this.writeBool = writeBool;
+      this.format = "YYYY-MM-DD HH:mm:ss";
+      const date = new Date();
+      this.fileName = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.log`;
+      this.logsDir = logsDir;
+      this.writeToFile(`[INFO] Logger initialized`);
+    }
+  }
+  debug(message: string, ...args: any[]): void {
+    if (process.env.NODE_ENV === "development") {
+      console.log(`[DEBUG] ${message}`, ...args);
+    }
+  }
+  error(message: string, ...args: any[]): void {
+    console.error(`[ERROR] ${message}`, ...args);
+    if (this.writeBool) {
+      this.writeToFile(`[ERROR] ${message}`, ...args);
+    }
+  }
+  log(message: string, ...args: any[]): void {
+    console.log(`[INFO] ${message}`, ...args);
+    if (this.writeBool) {
+      this.writeToFile(`[INFO] ${message}`, ...args);
+    }
+  }
+  warn(message: string, ...args: any[]): void {
+    console.warn(`[WARN] ${message}`, ...args);
+    if (this.writeBool) {
+      this.writeToFile(`[WARN] ${message}`, ...args);
+    }
+  }
+  setFormat(format: string): void {
+    this.format = format;
+  }
+  writeToFile(message: string, ...args: any[]): void {
+    let { fileName } = this;
+    const date = new Date();
+    const filePath = `${process.cwd()}/${this.logsDir}/${fileName}.log`;
+    if (!existsSync(filePath)) {
+      appendFileSync(filePath, "", "utf8");
+    }
+    writeFileSync(
+      filePath,
+      `${this.format
+        .replace("YYYY", date.getFullYear().toString())
+        .replace("MM", date.getMonth().toString())
+        .replace("DD", date.getDate().toString())} ${this.format
+        .replace("HH", date.getHours().toString())
+        .replace("mm", date.getMinutes().toString())
+        .replace("ss", date.getSeconds().toString())} ${message}\n`,
+      { flag: "a" }
+    );
+  }
 }
