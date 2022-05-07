4948980714e7d2d106ae6429288f4838630d6ec2
Birahe <ardaaozyigit@gmail.com>
2022-05-07 13:13:09 +0300

    Command Handler without Events

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,11 +1,15 @@
-import { Config } from "./../Interfaces";
+import { Config } from "../Interfaces";
 import { Client, ClientOptions } from "discord.js";
 import Logger from "../Logger";
+import { CommandHandler } from "../Handlers";
 import config from "../Config";
+import { readdirSync } from "fs";
+import { join } from "path";
 
 export default class EarestClient extends Client {
-  public logger: Logger = new Logger(true, "logs");
+  public logger: Logger = new Logger(false, "logs");
   public config: Config = config;
+  public commandHandler: CommandHandler = new CommandHandler(this);
   constructor(params: ClientOptions) {
     super(params);
   }
@@ -13,5 +17,18 @@ export default class EarestClient extends Client {
   public start(): void {
     this.logger.log("Starting Earest...");
     this.login(this.config.token);
+  }
+
+  public registerCommands(): void {
+    readdirSync(join(__dirname, "..", "Commands")).forEach((dir) => {
+      const commands = readdirSync(
+        join(__dirname, "..", "Commands", dir)
+      ).filter((file) => file.endsWith(".ts"));
+      for (const command of commands) {
+        this.commandHandler.load(
+          join(__dirname, "..", "Commands", dir, command)
+        );
+      }
+    });
   }
 }
---
+++ src/Handlers/CommandHandler.ts
@@ -0,0 +1,34 @@
+import EarestClient from "Client";
+import { Command, CommandHandler as ICommandHandler } from "Interfaces";
+
+export class CommandHandler implements ICommandHandler {
+  public commands: Command[] = [];
+  constructor(public client: EarestClient) {}
+  public load(fileName: string) {
+    const command = require(fileName).default;
+    try {
+      if (!this.client.application) return;
+      if (!command) return;
+      this.client.logger.debug(`Registering command ${command.name}`);
+      this.commands.push(command);
+      if (process.env.NODE_ENV == "development")
+        return this.client.guilds.cache
+          .get("942107219242147931")
+          ?.commands.set(this.commands as any);
+      this.client.application.commands.set(this.commands as any);
+    } catch (e) {
+      this.client.logger.warn("Failed to load command " + command.name);
+    } finally {
+      this.client.logger.log("Loaded command " + command.name);
+    }
+  }
+  public unload(fileName: string): void {}
+  public reload(fileName: string): void {}
+  public getCommand(name: string): Command | null {
+    return this.commands.find((c) => c.name === name) || null;
+  }
+  public hasCommand(name: string): boolean {
+    if (this.commands.find((c) => c.name === name)) return true;
+    return false;
+  }
+}
